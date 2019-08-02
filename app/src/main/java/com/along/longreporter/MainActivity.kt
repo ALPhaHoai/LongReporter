f0d7c435801287200608bc6f3bf59cd353781a19
Bruce <alphahoai@gmail.com>
2019-08-02 10:15:19 +0700

    update project

--- app/src/main/java/com/along/longreporter/MainActivity.kt
+++ app/src/main/java/com/along/longreporter/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var email = "alphahoai@gmail.com"
+    var email2 = "ngoclonghk96@gmail.com"
     var menu: Menu? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +17,13 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
 
-            email = "ngoclonghk96@gmail.com"
-            menu?.findItem(R.id.action_user_email)?.setTitle(email)
+            val userEmailItem = menu?.findItem(R.id.action_user_email)
+            if (userEmailItem != null) {
+                userEmailItem.title = if (userEmailItem.title.equals(email)) email2 else email
+            }
         }
     }
 
@@ -28,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         this.menu = menu
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
-        menu.findItem(R.id.action_user_email).setTitle(email)
+        menu.findItem(R.id.action_user_email).title = email
         return true
     }
 
@@ -38,27 +41,27 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_notifications -> {
-                Toast.makeText(this, "Notification", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
                 return true
             }
             R.id.action_text_size -> {
-                Toast.makeText(this, "Text Size", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
                 return true
             }
             R.id.action_send_app_feedback -> {
-                Toast.makeText(this, "Send App Feedback", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
                 return true
             }
             R.id.action_settings -> {
-                Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
                 return true
             }
             R.id.action_user_email -> {
-                Toast.makeText(this, "User email", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
                 return true
             }
             R.id.action_subscribe -> {
-                Toast.makeText(this, "Subscribe", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
                 return true
             }
             else -> {
