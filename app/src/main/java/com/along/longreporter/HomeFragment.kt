ad1ccb84b3fa71eb52fcb9e4371d83613dc02429
Long <alphahoai@gmail.com>
2019-08-03 13:19:42 +0700

    add pulltorefresh to load more data

--- app/src/main/java/com/along/longreporter/HomeFragment.kt
+++ app/src/main/java/com/along/longreporter/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 /**
  * Created by Long
@@ -18,13 +19,17 @@ class HomeFragment : Fragment() {
     private var v: View? = null
     private var progressBar: ProgressBar? = null
     private var recycleView: RecyclerView? = null
+    private var swipeRefresh: SwipeRefreshLayout? = null
     public val articleList: ArrayList<ArticleModel> = ArrayList()
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         v = inflater.inflate(R.layout.home_fragment, container, false)
         recycleView = v?.findViewById(R.id.recycleView)
         progressBar = v?.findViewById(R.id.progressBar)
+        swipeRefresh = v?.findViewById(R.id.swiperefresh)
+        swipeRefresh?.setOnRefreshListener({
+            loadData()
+        })
         recycleView?.layoutManager = LinearLayoutManager(this.context)
         recycleView?.adapter = ArticleAdapter(articleList, this.context!!)
         loadData()
@@ -32,11 +37,18 @@ class HomeFragment : Fragment() {
     }
 
     fun loadData() {
-        progressBar?.visibility = View.VISIBLE
+        val isRefreshing = swipeRefresh?.isRefreshing ?: false
+        if (!isRefreshing) {
+            progressBar?.visibility = View.VISIBLE
+        }
         CrawlData() {
             articleList.addAll(it)
             recycleView?.adapter?.notifyDataSetChanged()
-            progressBar?.visibility = View.GONE
+            if (isRefreshing) {
+                swipeRefresh?.setRefreshing(false)
+            } else {
+                progressBar?.visibility = View.GONE
+            }
         }.execute()
     }
 }
